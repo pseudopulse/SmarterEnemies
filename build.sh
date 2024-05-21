af8811dfc9dc102ffb103883dc48b84c9914d0c8
psuedopulse <psuedopulse@protonmail.com>
2024-05-21 11:13:50 -0500

    guh

--- build.sh
+++ build.sh
@@ -3,3 +3,14 @@ dotnet restore
 dotnet build
 rm -rf ~/.config/r2modmanPlus-local/RiskOfRain2/profiles/SmarterEnemies/BepInEx/plugins/SmarterEnemies
 cp -r SmarterEnemies/bin/Debug/netstandard2.0  ~/.config/r2modmanPlus-local/RiskOfRain2/profiles/SmarterEnemies/BepInEx/plugins/SmarterEnemies
+
+rm -rf SEBuild
+mkdir SEBuild
+cp icon.png SEBuild
+cp manifest.json SEBuild
+cp README.md SEBuild
+cp SmarterEnemies/bin/Debug/netstandard2.0/SmarterEnemies.dll SEBuild
+cd SEBuild
+rm ../SE.zip
+zip ../SE.zip *
+cd ..
